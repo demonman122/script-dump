fed716e029badd4bd1d952805a3d5bdd4e817a6e
frosted <66559863+notfrosted@users.noreply.github.com>
2022-04-03 12:33:37 +0100

    Update dupe setup.lua

--- dupe setup.lua
+++ dupe setup.lua
@@ -1,5 +1,4 @@
 --[[
-
 ███╗░░░███╗░█████╗░░█████╗░███╗░░██╗░██╗░░░░░░░██╗░█████╗░██████╗░███████╗
 ████╗░████║██╔══██╗██╔══██╗████╗░██║░██║░░██╗░░██║██╔══██╗██╔══██╗██╔════╝
 ██╔████╔██║██║░░██║██║░░██║██╔██╗██║░╚██╗████╗██╔╝███████║██████╔╝█████╗░░
@@ -7,9 +6,7 @@
 ██║░╚═╝░██║╚█████╔╝╚█████╔╝██║░╚███║░░╚██╔╝░╚██╔╝░██║░░██║██║░░██║███████╗
 ╚═╝░░░░░╚═╝░╚════╝░░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░╚═╝░░╚═╝░░╚═╝╚═╝░░╚═╝╚══════╝
 ]]
-
 repeat task.wait() until game:IsLoaded() pcall(function() repeat wait() until game.Players.LocalPlayer.Character:FindFirstChild('FULLY_LOADED_CHAR') end) if not game:IsLoaded() then usingAutoExecute = true repeat wait() until game:IsLoaded() end
-
 coroutine.resume(coroutine.create(function() game.StarterGui:SetCore("SendNotification", { Title = "dupe"; Text = "made by notfrosted#9999";Duration = "5" }) game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("swagmode loading", 'All'); wait(2)
 game.ReplicatedStorage.DefaultChatSystemChatEvents.SayMessageRequest:FireServer("enabled cash aura + godmode", 'All'); function Godmode() if game:GetService("Players").LocalPlayer.Character.BodyEffects:FindFirstChild("Attacking") then game:GetService("Players").LocalPlayer.Character.BodyEffects:FindFirstChild("Attacking"):Destroy() end end
 Godmode() while v and wait() do for _,v in pairs(workspace.Ignored.Drop:GetChildren()) do if v:IsA('Part') and v.Name == "MoneyDrop" then pcall(function() if (player.Character.HumanoidRootPart.Position - v.Position).Magnitude <= 100 then fireclickdetector(v:WaitForChild('ClickDetector')) end end) end end end
